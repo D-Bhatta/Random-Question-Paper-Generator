6d288861da77fe88dbe709318273401503aa22e8
DB <dbhatta1232@gmail.com>
2019-02-16 07:21:15 -0500

    Create index

--- webpages/index.php
+++ webpages/index.php
@@ -1,56 +1,207 @@
 <?php include("header.php"); ?>
-<table width="100%" border="0">
-        <tr>
-            <td>
-                <img src="../images/Banner/Methodologies.jpg" width="100%" height="80%">
-            </td>
-        </tr>
-    </table>
-    <table width=100%>
-        <tr>
-            <td>
-                   <br> <button type="button" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-edit"></span><a href="Generate%20Question%20paper.php"> Generate Question Paper</a></button>
-                   <button type="button" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-pencil"></span> Report</button>
-                   <button type="button" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-log-in"></span> Insert Questions</button>
-                </td>
-        </tr>
-    </table>
-
-<table width=100%>
-    <tr>
-        <td>
-                <div class="container-fluid">
-                        <h1><center>Developed by:</center></h1>
-                        <div class="row">
-                                <div class="col-sm-6" style="background-color:lavender;text-align:right;">Debabrata Bhattacharya
-                                <br>Poulamee Thakur
-                            <br>Eshani Das
-                        <br>Trisha Ghosh
-                         <br>Bipasha Brahma
-                        <br>Abhishek Singh
-                    <br>Riya Nandy
-                <br>Kaustab Roy</div>
-                                <div class="col-sm-6" style="background-color:lavenderblush;text-align:left;">161112015004
-                                <br>161212015009<br>161212015011<br>161212015014<br>161212015026<br>161112015019<br>17<br>161112015005</div>
+ <div class="main"> 
+	<div class="carousel-container">
+			<h2>Carousel Example</h2>  
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			  </ol>
+		  
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner">
+				<div class="item active">
+				  <img src="../images/carousel1.png" alt="icon" style="width:100%;">
+				  	<div class="carousel-caption">
+						<h3>Web Education Institute</h3>
+						<p>Always Ahead</p>
+					</div>
+				</div>
+		  
+				<div class="item">
+				  <img src="../images/carousel2.jpg" alt="stem_workshop" style="width:100%;">
+				  	<div class="carousel-caption">
+						<h3>Stem Workshop</h3>
+						<p>Stem Workshop was so much fun!</p>
+					</div>
+				</div>
+			  
+				<div class="item">
+				  <img src="../images/carousel3.jpg" alt="philosophy_workshop" style="width:100%;">
+				</div>
+				<div class="item">
+					<img src="../images/carousel4.jpg" alt="professional_development_workshop" style="width:100%;">
+						<div class="carousel-caption">
+							<h3>Professional Development Workshop</h3>
+							<p>We learn how to exist in a professional world</p>
+						</div>
+				</div>
+			
+				<div class="item">
+					<img src="../images/carousel5.jpg" alt="taiwan_cooperative_workshop" style="width:100%;">
+						<div class="carousel-caption">
+							<h3>Taiwan Cooperative Workshop</h3>
+							<p>Taiwan Cooperative Workshop starts the lab in Taiwan</p>
+						</div>
+				</div>
+				<div class="item">
+					<img src="../images/carousel6.jpg" alt="taiwan_cooperative2_workshop" style="width:100%;">
+						<div class="carousel-caption">
+							<h3>Taiwan Cooperative Workshop</h3>
+							<p>Taiwan Cooperative Workshop-at the end of a long day!</p>
+						</div>
+				</div>
+			  </div>
+		  
+			  <!-- Left and right controls -->
+			  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			  </a>
+			</div>
+		  </div>
+	<div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-2 mx-auto">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body mb-2">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+            </div>
+            <div class="col-2 mx-auto">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body mb-2">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
-                      </div>
-        </td>
-    </tr>
-</table>
+                    </div>
+            </div>
+        </div>
+        <div class="row justify-content-md-center">
+                <div class="col-2 mx-auto">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body mb-2">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                </div>
+                </div>
+                <div class="col-2 mx-auto">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body mb-2">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                </div>
+        </div>
+         
+        <div class="row justify-content-md-center">
+                <div class="col-2 mx-auto">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body mb-2">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                </div>
+                </div>
+                <div class="col-2 mx-auto">
+                        <div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body mb-2">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                </div>
+        </div>
+        
+        
+        
+        
+        
+        
+        <!--                                      
+			<a href="index.php">
+			<div class="contents-item">
+			<img src="../images/html.svg">
+			<p>Home</p>
+			</div>
+			</a>
+			
+			<a href="about_us.php">
+			<div class="contents-item">
+			<img src="../images/php.svg">
+			<p>About Us</p>
+			</div>	
+			</a>
+			 
+			<a href="courses.php">
+			<div class="contents-item">
+			<img src="../images/ruby.svg">
+			<p>Courses</p>
+			</div>
+			</a>
+			
+			<a href="academic.php">
+			<div class="contents-item">
+			<img src="../images/swift.svg">
+			<p>Academic</p>
+			</div>
+			</a>
+	</div>
+	
+	
+	
+ 
 
-    <table width="100%" border="0">
-            <tr style="background-color: black"  bordercolor="#000099">
-              
-            <td style="text-align:center; font-size:24px; font-weight:bold;">
-            <span style="color:#FFFFFF;">Developed By students of JISU</span><br/></td>
-           
-            </tr>
-            </table>
-            
-                
-</td>
-</tr>
-</table>
+ </div>
 
-    </body>
-    <.php>
+	<div class="footer">
+	
+		
+		<div class="footer-logo">
+			Web Education Institute
+		</div>
+		
+		
+		
+		<div class="footer-list">
+		
+	
+		
+		<ul>
+			<li>
+				<a href="aboutus.php">About</a>
+			</li>
+			<li>
+				<a href="courses.php">Courses</a>
+			</li>
+			<li>
+				<a href="contact.php">Contact Us</a>
+			</li>
+		</ul>
+		
+		</div>
+	</div> 
+</body> 
+</html>
