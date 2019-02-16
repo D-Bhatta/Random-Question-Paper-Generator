a59e40a6da615cdf99e4c098b222f347c238b77e
DB <dbhatta1232@gmail.com>
2019-02-16 08:05:02 -0500

    Altered Index

--- webpages/index.php
+++ webpages/index.php
@@ -67,32 +67,82 @@
 			</div>
 		  </div>
 	<div class="container">
-        <div class="row justify-content-md-center">
+        <div class="row">
             <div class="col-2 mx-auto">
                 <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <a href="login.php"><img class="card-img-top" src="../images/Fuser.png" alt="Card image cap"></a>
                     <div class="card-body mb-2">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title">Login As User</h5>
+                        <p class="card-text">Login to generate your own Question Paper and Get access to analytics</p>
+                        <a href="login.php" class="btn btn-primary">Login</a>
                     </div>
             </div>
             </div>
             <div class="col-2 mx-auto">
                     <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <a href="login_admin.php"><img class="card-img-top" src="../images/admin.png" alt="Card image cap"></a>
                         <div class="card-body mb-2">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h5 class="card-title">Login As Admin</h5>
+                            <p class="card-text">Login as System Admin to view user reports, Allow insertion of questions and much more!</p>
+                            <a href="login_admin.php" class="btn btn-primary">Login</a>
                         </div>
                     </div>
             </div>
         </div>
-        <div class="row justify-content-md-center">
+        <div class="col-sm-4">
+         </div>
+        <div class="row">
                 <div class="col-2 mx-auto">
                     <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <a href="genqpaper.php"><img class="card-img-top" src="../images/paper.png" alt="Card image cap"></a>
+                        <div class="card-body mb-2">
+                            <h5 class="card-title">Generate</h5>
+                            <p class="card-text">Generate a random question paper!</p>
+                            <a href="genqpaper.php" class="btn btn-primary">Generate</a>
+                        </div>
+                </div>
+                </div>
+                <div class="col-2 mx-auto">
+                        <div class="card" style="width: 18rem;">
+                            <a href="download2.php"><img class="card-img-top" src="../images/pdf_download.png" alt="Card image cap"></a>
+                            <div class="card-body mb-2">
+                                <h5 class="card-title">Download</h5>
+                                <p class="card-text">Download last generated question paper!</p>
+                                <a href="download2.php" class="btn btn-primary">Download</a>
+                            </div>
+                        </div>
+                </div>
+        </div>
+        <div class="col-sm-4">
+        </div>
+        <div class="row">
+                <div class="col-2 mx-auto">
+                    <div class="card" style="width: 18rem;">
+                        <a href="insqs.php"><img class="card-img-top" src="../images/tickmark.jpg" alt="Card image cap"></a>
+                        <div class="card-body mb-2">
+                            <h5 class="card-title">Insert Questions</h5>
+                            <p class="card-text">Insert Your own Questions into the database!</p>
+                            <a href="insqs.php" class="btn btn-primary">Insert</a>
+                        </div>
+                </div>
+                </div>
+                <div class="col-2 mx-auto">
+                        <div class="card" style="width: 18rem;">
+                            <a href="report.php"><img class="card-img-top" src="../images/curve.gif" alt="Card image cap"></a>
+                            <div class="card-body mb-2">
+                                <h5 class="card-title">Report</h5>
+                                <p class="card-text">Submit a report on the statistics of your exam!</p>
+                                <a href="report.php" class="btn btn-primary">Report</a>
+                            </div>
+                        </div>
+                </div>
+        </div>
+        <div class="col-sm-4">
+        </div>
+        <div class="row">
+                <div class="col-2 mx-auto">
+                    <div class="card" style="width: 18rem;">
+                        <a href="#"><img class="card-img-top" src="../images/" alt="Card image cap"></a>
                         <div class="card-body mb-2">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -102,7 +152,7 @@
                 </div>
                 <div class="col-2 mx-auto">
                         <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <a href="#"><img class="card-img-top" src="../images/" alt="Card image cap"></a>
                             <div class="card-body mb-2">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -111,30 +161,6 @@
                         </div>
                 </div>
         </div>
-         
-        <div class="row justify-content-md-center">
-                <div class="col-2 mx-auto">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body mb-2">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                </div>
-                </div>
-                <div class="col-2 mx-auto">
-                        <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="..." alt="Card image cap">
-                            <div class="card-body mb-2">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                </div>
-        </div>
-        
         
         
         
