08a76acb31ef69aa7115cd8531eb87c856319df5
DB <dbhatta1232@gmail.com>
2019-02-16 09:05:16 -0500

    Altered Index

--- webpages/index.php
+++ webpages/index.php
@@ -1,71 +1,68 @@
 <?php include("header.php"); ?>
  <div class="main"> 
-	<div class="carousel-container">
-			<h2>Carousel Example</h2>  
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-			  <!-- Indicators -->
-			  <ol class="carousel-indicators">
-				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-				<li data-target="#myCarousel" data-slide-to="1"></li>
-				<li data-target="#myCarousel" data-slide-to="2"></li>
-			  </ol>
-		  
-			  <!-- Wrapper for slides -->
-			  <div class="carousel-inner">
-				<div class="item active">
-				  <img src="../images/carousel1.png" alt="icon" style="width:100%;">
-				  	<div class="carousel-caption">
-						<h3>Web Education Institute</h3>
-						<p>Always Ahead</p>
-					</div>
-				</div>
-		  
-				<div class="item">
-				  <img src="../images/carousel2.jpg" alt="stem_workshop" style="width:100%;">
-				  	<div class="carousel-caption">
-						<h3>Stem Workshop</h3>
-						<p>Stem Workshop was so much fun!</p>
-					</div>
-				</div>
-			  
-				<div class="item">
-				  <img src="../images/carousel3.jpg" alt="philosophy_workshop" style="width:100%;">
-				</div>
-				<div class="item">
-					<img src="../images/carousel4.jpg" alt="professional_development_workshop" style="width:100%;">
-						<div class="carousel-caption">
-							<h3>Professional Development Workshop</h3>
-							<p>We learn how to exist in a professional world</p>
-						</div>
-				</div>
-			
-				<div class="item">
-					<img src="../images/carousel5.jpg" alt="taiwan_cooperative_workshop" style="width:100%;">
-						<div class="carousel-caption">
-							<h3>Taiwan Cooperative Workshop</h3>
-							<p>Taiwan Cooperative Workshop starts the lab in Taiwan</p>
-						</div>
-				</div>
-				<div class="item">
-					<img src="../images/carousel6.jpg" alt="taiwan_cooperative2_workshop" style="width:100%;">
-						<div class="carousel-caption">
-							<h3>Taiwan Cooperative Workshop</h3>
-							<p>Taiwan Cooperative Workshop-at the end of a long day!</p>
-						</div>
-				</div>
-			  </div>
-		  
-			  <!-- Left and right controls -->
-			  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left"></span>
-				<span class="sr-only">Previous</span>
-			  </a>
-			  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right"></span>
-				<span class="sr-only">Next</span>
-			  </a>
-			</div>
-		  </div>
+    <div id="carouselObject" class="carousel slide " data-ride="carousel" style="width: 500px; height: 500px; margin: 0  auto">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselObject" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselObject" data-slide-to="1"></li>
+            <li data-target="#carouselObject" data-slide-to="2"></li>
+            <li data-target="#carouselObject" data-slide-to="3"></li>
+            <li data-target="#carouselObject" data-slide-to="4"></li>
+        </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="../images/logo.png" alt="">
+            <div class="carousel-caption d-none d-md-block text-primary bg-light">
+                    <h5>Random Question Paper Generator</h5>
+                    <p>Fast random question papers in PDF format</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+                <img class="d-block w-100" src="../images/logo.png" alt="">
+                <div class="carousel-caption d-none d-md-block">
+                        <h5>Built by JIS University Students</h5>
+                        <p></p>
+                </div>
+        </div>
+        <div class="carousel-item">
+                <img class="d-block w-100" src="../images/paper.png" alt="">
+                <div class="carousel-caption d-none d-md-block text-primary">
+                        <h5>Generate Question Paper</h5>
+                        <p>Generate in seconds from a large question bank</p>
+                </div>
+        </div>
+        <div class="carousel-item">
+                <img class="d-block w-100" src="../images/pdf_download.png" alt="">
+                <div class="carousel-caption d-none d-md-block">
+                        <h5>Download</h5>
+                        <p>Download in PDF format!</p>
+                </div>
+        </div>
+        <div class="carousel-item">
+                <img class="d-block w-100" src="../images/tickmark.jpg" alt="">
+                <div class="carousel-caption d-none d-md-block">
+                        <h5>Insert Questions</h5>
+                        <p>Insert Your Own Questions!</p>
+                </div>
+        </div>
+        <div class="carousel-item">
+                <img class="d-block w-100" src="../images/curve.gif" alt="">
+                <div class="carousel-caption d-none d-md-block">
+                        <h5>Report</h5>
+                        <p>Submit statistics!</p>
+                </div>
+        </div>
+    </div>
+        <a class="carousel-control-prev" href="#carouselObject" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselObject" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    
 	<div class="container">
         <div class="row">
             <div class="col-2 mx-auto">
@@ -161,46 +158,15 @@
                         </div>
                 </div>
         </div>
-        
-        
-        
-        
-        
-        <!--                                      
-			<a href="index.php">
-			<div class="contents-item">
-			<img src="../images/html.svg">
-			<p>Home</p>
-			</div>
-			</a>
-			
-			<a href="about_us.php">
-			<div class="contents-item">
-			<img src="../images/php.svg">
-			<p>About Us</p>
-			</div>	
-			</a>
-			 
-			<a href="courses.php">
-			<div class="contents-item">
-			<img src="../images/ruby.svg">
-			<p>Courses</p>
-			</div>
-			</a>
-			
-			<a href="academic.php">
-			<div class="contents-item">
-			<img src="../images/swift.svg">
-			<p>Academic</p>
-			</div>
-			</a>
-	</div>
+    </div>
+</div>
+
 	
 	
 	
  
+<!--
 
- </div>
 
 	<div class="footer">
 	
