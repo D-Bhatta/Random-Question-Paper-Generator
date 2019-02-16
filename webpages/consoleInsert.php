efc21b1f0d05b9078a972604b88e94ebf749e4ac
DB <dbhatta1232@gmail.com>
2019-02-16 03:54:46 -0500

    Added Admin Console

---
+++ webpages/consoleInsert.php
@@ -0,0 +1,57 @@
+<?php include("header.php"); ?>
+<div class="main">
+ <?php
+
+
+
+# Init the MySQL Connection
+$servername = "localhost";
+$username = "testuser";
+$password = "test123";
+$database = "testdb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+# Prepare the SELECT Query
+ $sql = 'SELECT * FROM `insertqs`';
+# Execute the SELECT Query
+ if( !( $result = $conn->query($sql) ) ){
+   echo 'Retrieval of data from Database Failed - #'.mysql_errno().': '.mysql_error();
+ }else{
+   ?>
+<table class="table">
+ <thead>
+   <tr>
+	    <th>Question.</th>
+        <th>Option 1</th>
+        <th>Option 2</th>
+        <th>Option 3</th>
+        <th>Option 4</th>
+        <th>Answer</th>
+        <th>Marks</th>
+	    <th>Difficulty</th>
+        <th>Subject</th>
+   </tr>
+ </thead>
+ <tbody>
+   <?php
+     if( $result->num_rows == 0 ){
+       echo '<tr><td colspan="4">No Rows Returned</td></tr>';
+     }else{
+       while( $row = $result->fetch_array(MYSQLI_ASSOC) ){
+				 echo "<tr><td>{$row['slno']}</td><td>{$row['Question']}</td><td>{$row['Option1']}</td><td>{$row['Option2']}</td><td>{$row['Option3']}</td><td>{$row['Option4']}</td><td>{$row['Answer']}</td><td>{$row['marks']}</td><td>{$row['difficulty']}</td><td>{$row['subject']}</td></tr>\n";
+       }
+     }
+   ?>
+ </tbody>
+</table>
+   <?php
+ }
+
+?>
---
+++ webpages/consoleReport.php
@@ -0,0 +1,54 @@
+<?php include("header.php"); ?>
+<div class="main">
+ <?php
+
+
+
+# Init the MySQL Connection
+$servername = "localhost";
+$username = "testuser";
+$password = "test123";
+$database = "testdb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+# Prepare the SELECT Query
+ $sql = 'SELECT * FROM `reports`';
+# Execute the SELECT Query
+ if( !( $result = $conn->query($sql) ) ){
+   echo 'Retrieval of data from Database Failed - #'.mysql_errno().': '.mysql_error();
+ }else{
+   ?>
+<table class="table">
+ <thead>
+   <tr>
+	 	 <th>Index.</th>
+     <th>User Name</th>
+     <th>Number of Students</th>
+     <th>Average Marks</th>
+	<th>Highest Marks</th>
+    <th>Lowest Marks</th>
+   </tr>
+ </thead>
+ <tbody>
+   <?php
+     if( $result->num_rows == 0 ){
+       echo '<tr><td colspan="4">No Rows Returned</td></tr>';
+     }else{
+       while( $row = $result->fetch_array(MYSQLI_ASSOC) ){
+				 echo "<tr><td>{$row['slno']}</td><td>{$row['username']}</td><td>{$row['nstudents']}</td><td>{$row['amarks']}</td><td>{$row['hmarks']}</td><td>{$row['lmarks']}</td></tr>\n";
+       }
+     }
+   ?>
+ </tbody>
+</table>
+   <?php
+ }
+
+?>
