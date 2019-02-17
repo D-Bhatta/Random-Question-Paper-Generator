8d7d7fb6c5d1e2ecbf082a9a5e26ca83525fad88
DB <dbhatta1232@gmail.com>
2019-02-17 05:54:48 -0500

    Create footer

---
+++ webpages/footer.php
@@ -0,0 +1,103 @@
+
+<?php
+
+$footer=<<<'EOD'
+<footer class="page-footer font-small bg-primary pt-4 fixed-bottom">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left text-white">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+
+          <!-- Content -->
+          <h5 class="text-uppercase">Random Question Paper Generator</h5>
+          <p>Created by students of JISU</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+            <!-- Links -->
+            <h5 class="navbar-brand">Links</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a class="text-white" href="genqpaper.php">Generate<span class="sr-only">(current)</span></a>
+              <li>
+                <a class="text-white" href="insqs.php">Insert</a>
+              </li>
+              <li>
+                <a class="text-white" href="report.php">Report</a>
+              </li>
+              <li>
+                <a class="text-white" href="consoleUser.php">User Console</a>
+              </li>
+              <li>
+                <a class="text-white" href="consoleAdmin.php">Admin Console</a>
+              </li>
+            </ul>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 mb-md-0 mb-3">
+
+            <!-- Links -->
+            <h5 class="text-uppercase">Team</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a class="text-white" href="#!">Debabrata</a>
+              </li>
+              <li>
+                <a class="text-white" href="#!">Poulamee</a>
+              </li>
+              <li>
+                <a class="text-white" href="#!">Eshani</a>
+              </li>
+              <li>
+                <a class="text-white" href="#!">Abhishek</a>
+              </li>
+              <li>
+                <a class="text-white" href="#!">Bipasha</a>
+              </li>
+              <li>
+                <a class="text-white" href="#!">Riya</a>
+              </li>
+              <li>
+                <a class="text-white" href="#!">Trisha</a>
+              </li>
+              <li>
+                <a class="text-white" href="#!">Koustab</a>
+              </li>
+            </ul>
+
+          </div>
+          <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 text-white">© 2018-19 Copyright:
+      <a class="text-white" href="https://github.com/D-Bhatta/Random-Question-Paper-Generator"> Random-Question-Paper-Generator</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
+EOD;
+
+echo $footer;
